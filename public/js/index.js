8213871354bfc73223df766d2dce8fc0b95f7cb1
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2018-10-30 14:54:03 -0500

    Added: Procfile, service worker

--- public/js/index.js
+++ public/js/index.js
@@ -6,3 +6,15 @@ document.addEventListener('DOMContentLoaded', function() {
     var carousels = document.querySelectorAll('.carousel');
     var instCarousels = M.Carousel.init(carousels);
 });
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+}
