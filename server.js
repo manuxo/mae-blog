b5a607fdce7d16ffb6ea1271b7aee1154a355dea
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2018-10-30 11:48:13 -0500

    lf index

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 //View engine
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname,'public'));
 app.set('view engine','ejs');
 
 //Config - CORS
