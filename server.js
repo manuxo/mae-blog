19e8e1a2535b232bab77b5e3dc55a33f28ba458e
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2018-10-30 11:41:44 -0500

    Added server and cors

--- server.js
+++ server.js
@@ -1,2 +1,50 @@
+//Dependencies
+const http = require('http');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+//Init App
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+//View engine
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+//Config - CORS
+const allowCORS = require('./middleware/cors');
+app.use(allowCORS);
+
+//Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan('dev'));
+
+//Public
+app.use(express.static(path.join(__dirname,'public')));
+
+//Routes
+
+app.get('/', (req,res) => {
+    res.status(200).render('index',{message:'index works!'});
+});
+
+//Error handling
+
+app.use((err,req,res,next) => {
+    if(err.status){
+        res.status(err.status).send(err.message);
+    }else{
+        res.status(500).send();
+    }
+});
+
+//Server
+const server = http.createServer(app);
+server.listen(PORT,() => {
+    console.log(`Server running at port: ${PORT}`);
+});
 
