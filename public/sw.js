0233c5194ce2a4cb62c9e075566c19bf1e318f69
Manuel Alvarado Estanga <alvarado.manuel@live.com>
2018-10-30 23:14:09 -0500

    sw fixed

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,22 @@
 var CACHE_NAME = 'cache1';
 var urlsToCache = [
-    'bundles.ejs',
-    'index.ejs',
-    'manifest.json'
+    '/css/style.css',
+    '/img/angular.png',
+    '/img/icon-192.png',
+    '/img/icon-512.png',
+    '/img/java.png',
+    '/img/mysql.png',
+    '/img/nodejs.png',
+    '/img/python.png',
+    '/img/user-background.png',
+    '/img/user.png',
+    '/js/index.js',
+    '/layouts/footer.ejs',
+    '/layouts/head.ejs',
+    '/layouts/header.ejs',
+    '/bundles.ejs',
+    '/index.ejs',
+    '/manifest.json'
 ];
 self.addEventListener('install', function(event) {
   // Perform install steps
